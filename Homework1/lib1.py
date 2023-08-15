cd249323fcd1f0f26ecf570cab121c96ed81cad7
Stonks i guess <122476274+StOnks6@users.noreply.github.com>
2023-08-15 12:55:47 +0200

    Delete Homework1 directory

--- Homework1/lib1.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict, List
-
-
-def get_affordable_products(products: List[Dict[str, int]], max_price: int) -> list[int]:
-    affordable_products = []
-    for product in products:
-        if product['price'] <= max_price:
-            affordable_products.append(product['product'])
-    return affordable_products
--- Homework1/lib2.py
+++
@@ -1,9 +0,0 @@
-from secrets import token_urlsafe
-
-END_PARTICLE = "END"  # The particle that should end the tape.
-
-
-def generate_tape(length=20):
-    tape = token_urlsafe(length)
-    tape += END_PARTICLE
-    return tape
