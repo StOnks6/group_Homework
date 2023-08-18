c90382c35f0dbc36a6fd21d587ffa8eed999e981
Stonks i guess <122476274+StOnks6@users.noreply.github.com>
2023-08-19 00:04:05 +0200

    Delete Test3 directory

--- Test3/filter.py
+++
@@ -1,6 +0,0 @@
-def is_valid_number(value):
-    return isinstance(value, (int)) and value > 500
-
-some_data = [501, 'fff', 50, 0, -50.5, 'bat', 600,358]
-
-filtered_data = list(filter(is_valid_number, some_data))
